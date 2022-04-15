f491a0a8fe4db6773d3f2eec68ad566cc65050b9
rambabu39 <rambabu.nalluri@gmail.com>
2022-04-15 16:03:58 +0530

    Update helloWorld.groovy

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,26 +1,26 @@
-import MyYaml
+//import MyYaml
 
 def call(Map config = [:]) {
     sh "echo Hello ${config.name}. Today is ${config.dayOfWeek}."
 }
-def Greet(name) {
-    echo "Hello ${name}"
-}
-def DisplayName() {
-    sh "echo Rambabu Nalluri running this script"
-}
-def dayOfWeek(Map config = [:]) {
-    def templates = []
-    sh "echo Hello ${config.name}. Today is ${config.dayOfWeek}."
-    List<String> args = [ 'a.yaml', 'b.yaml'];
-    delf templete1 = libraryResource('a.yaml')
-    delf templete2 = libraryResource('b.yaml')
+// def Greet(name) {
+//     echo "Hello ${name}"
+// }
+// def DisplayName() {
+//     sh "echo Rambabu Nalluri running this script"
+// }
+// def dayOfWeek(Map config = [:]) {
+//     def templates = []
+//     sh "echo Hello ${config.name}. Today is ${config.dayOfWeek}."
+//     List<String> args = [ 'a.yaml', 'b.yaml'];
+//     delf templete1 = libraryResource('a.yaml')
+//     delf templete2 = libraryResource('b.yaml')
     
-    templates.add(templete1)
-    templates.add(templete2)
+//     templates.add(templete1)
+//     templates.add(templete2)
     
-    def myyaml = new MyYaml()
-    def final_template = myyaml.merge(templates)
+//     def myyaml = new MyYaml()
+//     def final_template = myyaml.merge(templates)
     
-    println final_template
-}
+//     println final_template
+// }
