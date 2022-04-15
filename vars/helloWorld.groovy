06ff6313c10600bcc2fd11f7e609fed54a5424df
rambabu39 <rambabu.nalluri@gmail.com>
2022-04-15 16:38:20 +0530

    Update helloWorld.groovy

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -11,4 +11,7 @@ def DisplayName() {
 }
 def dayOfWeek(Map config = [:]) {
     sh "echo Hello ${config.name}. Today is ${config.dayOfWeek}."
+    def templete1 = libraryResource('a.yaml')
+    def templete2 = libraryResource('b.yaml')
+    println templete1+templete2
 }
