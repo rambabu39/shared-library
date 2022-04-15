945630448e63274fc72aba2916cbd2b0be854f3a
rambabu39 <rambabu.nalluri@gmail.com>
2022-04-15 15:48:00 +0530

    Update helloWorld.groovy

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -10,12 +10,17 @@ def DisplayName() {
     sh "echo Rambabu Nalluri running this script"
 }
 def dayOfWeek(Map config = [:]) {
-    
+    def templates = []
     sh "echo Hello ${config.name}. Today is ${config.dayOfWeek}."
     List<String> args = [ 'a.yaml', 'b.yaml'];
-
+    delf templete1 = libraryResource('a.yaml')
+    delf templete2 = libraryResource('b.yaml')
+    
+    templates.add(templete1)
+    templates.add(templete2)
+    
     def myyaml = new MyYaml()
-    def final_template = myyaml.merge(args)
+    def final_template = myyaml.merge(templates)
     
     println final_template
 }
